05e9fc1d05cb46587906e781558a04e2b9898575
felpgoes <felipe.goess@hotmail.com>
2022-07-28 20:14:30 -0300

    feat: adding git message and jest new jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,13 @@
-export default {
-    testMatch: ['<rootDir>/tests/*.test.ts'],
-    clearMocks: true,
-    preset: 'ts-jest',
-    verbose: true,
-    testEnvironment: 'node',
-    maxWorkers: 1,
-    collectCoverage: true,
-    testTimeout: 5000,
-    collectCoverageFrom: ['src/**/*.ts','!src/models/Model.ts', '!src/*', '!src/middlewares/*']
+module.exports = {
+  roots: ['<rootDir>/src'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/test/**',
+    '!**/config/**'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 }
